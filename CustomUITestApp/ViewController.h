335158a5cd37346b5611f2c5b623a3025c01852a
JJ <Admin@Admins-MBP-3.home>
2014-11-05 22:27:16 -0800

    Initial Commit

---
+++ CustomUITestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomUITestApp
+//
+//  Created by JJ on 11/5/14.
+//  Copyright (c) 2014 JayLei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
